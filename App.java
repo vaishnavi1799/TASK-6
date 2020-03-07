37bbaef331f3622bbca3d03fafffeb1e170d96f4
vaishnavi1799 <60377309+vaishnavi1799@users.noreply.github.com>
2020-03-07 20:19:46 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.archetypes.maven_archetype_quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
